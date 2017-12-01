9171372ddec6e5ffd90020b49d354c9e5e7cd5a0
Bruno Martins <bruno.martins@wunderman.com>
2017-12-01 19:02:28 -0200

    Adicionando modulo para cadastro, falta listagem

--- alura-admin/src/App.js
+++ alura-admin/src/App.js
@@ -1,53 +1,14 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import * as api from './service/RestApi.js';
-import Input from './components/forms/input.js';
-import Submit from './components/forms/submit.js';
+// import * as api from './service/RestApi.js';
+// import Input from './components/forms/input.js';
+// import Submit from './components/forms/submit.js';
+import {RegisterForm, ListTable} from './modules/form.js'
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      authorList : [],
-      value: '',
-      nome : '',
-      email : '',
-      pass : ''
-  };
-
-    this.getAll = this.getAll.bind(this);
-    this.setAuthor = this.setAuthor.bind(this);
-
-    this.getAll();
-  }
-
-  handleChange(fieldName) {
-      return function (event){
-          this.setState({[fieldName]: event.target.value});        
-      }
-  }
-
-  async setAuthor (e){
-    e.preventDefault();
-    const setResponse = await api.setAuthor(this.state.nome, this.state.email, this.state.pass);
-        if(setResponse.status === 201){
-          this.getAll();
-        }
-  }
-  async getAll (){
-    const getAllRegister = await api.getAuthor();
-    const arr = [];
-    let result;
-
-    if (getAllRegister.status === 200){
-      result = getAllRegister.data.results
-      console.log(result);
-      result.forEach((item) => {
-        arr.push(item)
-      });
-        this.setState({authorList : arr});
-    }
   }
 
   render() {
@@ -74,17 +35,20 @@ class App extends Component {
                     <h1>Cadastro de Autores</h1>
                   </div>
                   <div className="content" id="content">
-                    
-                    <div className="pure-form pure-form-aligned">
+
+                    <RegisterForm/>
+                    <ListTable/>
+
+                    {/*<div className="pure-form pure-form-aligned">
                       <form className="pure-form pure-form-aligned">
                           <Input name="nome" label="Nome" id="nome" type="text" value={this.state.nome} onChange={this.handleChange('nome').bind(this)}/>
                           <Input name="email" label="Email" id="email" type="email" value={this.state.email} onChange={this.handleChange('email').bind(this)}/>
                           <Input name="senha" label="Senha" id="senha" type="password" value={this.state.pass} onChange={this.handleChange('pass').bind(this)}/>
                           <Submit className="pure-button pure-button-primary" onClick={this.setAuthor} value="Cadastrar"/>
                       </form> 
-                    </div> 
+                    </div> */}
 
-                    <div>            
+                    {/*<div>            
                       <table className="pure-table">
                         <thead>
                           <tr>
@@ -105,7 +69,8 @@ class App extends Component {
                           }
                         </tbody>
                       </table> 
-                    </div>             
+                    </div>             */}
+
                   </div>
                 </div> 
               </div>
